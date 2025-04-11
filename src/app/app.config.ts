e05b7899ddbed54aeaf15249885be335c5c03964
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-04-10 23:07:50 -0700

    improve(core): Changes to install NgRx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, provideZoneChangeDetection, isDevMode} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {providePrimeNG} from 'primeng/config';
 import Nora from '@primeng/themes/nora';
+import { provideRouterStore } from '@ngrx/router-store';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,12 +16,16 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimations(),
     providePrimeNG({
-      theme: {
-        preset: Nora,
-        options: {
-          darkModeSelector: 'none',
+        theme: {
+            preset: Nora,
+            options: {
+                darkModeSelector: 'none',
+            }
         }
-      }
-    })
-  ]
+    }),
+    provideRouterStore(),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEffects()
+]
 };
